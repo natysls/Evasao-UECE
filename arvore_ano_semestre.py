cd65ad7ed463eb7ecd056879df5565dd7569d8d9
Natalia <naty-aragao@hotmail.com>
2023-11-28 00:12:35 -0300

    scatter plot

--- arvore_ano_semestre.py
+++ arvore_ano_semestre.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split, cross_val_score
 import matplotlib.pyplot as plt
@@ -16,19 +16,14 @@ df = df.loc[df['CD_SEM_INGRESSO'] == 2]
 df = df.drop(['CD_ANO_INGRESSO', 'CD_SEM_INGRESSO', 'DT_SIT_ALU', 'N_COLOCA', 'N_TOTESC', 'N_NOTRED'], axis=1)
 df = df.fillna(0)
 
-label_encoder = preprocessing.LabelEncoder()
-df['DS_BAIRRO'] = label_encoder.fit_transform(df['DS_BAIRRO'])
-mapping = dict(zip(label_encoder.classes_, range(len(label_encoder.classes_))))
+columns_mapping = ['DS_SIT_ALU', 'DS_BAIRRO', 'DS_CIDADE', 'DS_ESTADO']
+for coluna in columns_mapping:
+    label_encoder = preprocessing.LabelEncoder()
+    df[coluna] = label_encoder.fit_transform(df[coluna])
+    mapping = dict(zip(label_encoder.classes_, range(len(label_encoder.classes_))))
 
-df['DS_CIDADE'] = label_encoder.fit_transform(df['DS_CIDADE'])
-mapping = dict(zip(label_encoder.classes_, range(len(label_encoder.classes_))))
-
-df['DS_ESTADO'] = label_encoder.fit_transform(df['DS_ESTADO'])
-mapping = dict(zip(label_encoder.classes_, range(len(label_encoder.classes_))))
-
-data = df.to_numpy()
-X = data[:,1:]
-y = data[:,:1]
+X = df.iloc[:,1:]
+y = df.iloc[:,:1]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -37,17 +32,31 @@ model.fit(X_train, y_train)
 
 predictions = model.predict(X_test)
 accuracy = accuracy_score(y_test, predictions)
-report = classification_report(y_test, predictions)
 
 print("Acurácia do Modelo:", accuracy)
-print("Relatório de Classificação:\n", report)
-
-nomes_das_colunas_X = df.iloc[:,1:].columns.tolist()
-X_df = pd.DataFrame(X, columns=nomes_das_colunas_X) 
-
-nomes_das_colunas_y = df.iloc[:,:1].columns.tolist()
-y_df = pd.DataFrame(y, columns=nomes_das_colunas_y) 
-
+""""
 plt.figure(figsize=(15, 12))
-tree.plot_tree(model, filled=True, feature_names=X_df.columns, class_names=y_df['DS_SIT_ALU'].unique(), fontsize=8)
+tree.plot_tree(model, filled=True, feature_names=X.columns, class_names=y['DS_SIT_ALU'].unique(), fontsize=8)
+plt.show()
+"""
+
+coluna1 = 'aluno'
+coluna2 = 'DS_BAIRRO'
+y_pred = predictions.reshape(-1, 1)
+
+X_df_train = X_train[[coluna1, coluna2]]
+X_df_test = X_test[[coluna1, coluna2]]
+y_df_train = y_train.values.reshape(-1, 1)
+y_df_test = y_test.values.reshape(-1, 1)
+
+print(X_df_test[y_df_test != y_pred])
+
+fig, ax = plt.subplots()
+ax.scatter(x=X_train[coluna1], y=X_train[coluna2], c=y_df_train, alpha=0.9, cmap='viridis')
+ax.scatter(x=X_test[coluna1], y=X_test[coluna2], c=y_pred, alpha=0.6, cmap='viridis')
+ax.scatter(x=X_test[coluna1], y=X_test[coluna2], c=y_df_test, alpha=0.2, cmap='viridis')
+
+plt.xlabel(coluna1)
+plt.ylabel(coluna2)
+plt.title('Scatter Plot entre ' + coluna1 + ' e ' + coluna2)
 plt.show()
