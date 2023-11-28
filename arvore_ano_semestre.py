4a3bb469ca38d47e0e56c6177fcf3318917c5d6d
Natália <natalia.sales@uece.br>
2023-11-28 15:44:24 -0300

    novo mapeamento

--- arvore_ano_semestre.py
+++ arvore_ano_semestre.py
@@ -16,8 +16,8 @@ df = df.loc[df['CD_SEM_INGRESSO'] == 2]
 df = df.drop(['CD_ANO_INGRESSO', 'CD_SEM_INGRESSO', 'DT_SIT_ALU', 'N_COLOCA', 'N_TOTESC', 'N_NOTRED'], axis=1)
 df = df.fillna(0)
 
-mapeamento = {'TRANSFERIDO': 1, 'GRADUADO': 2, 'CURSANDO': 3, 'ABANDONO': 4,  'DESISTENTE': 5, 'CANCELADO': 6}
-df['DS_SIT_ALU'] = df['DS_SIT_ALU'].replace(mapeamento)
+evasao = {'TRANSFERIDO': 'NAO EVADIU', 'GRADUADO': 'NAO EVADIU', 'CURSANDO': 'NAO EVADIU', 'ABANDONO': 'EVADIU',  'DESISTENTE': 'EVADIU', 'CANCELADO': 'EVADIU'}
+df['DS_SIT_ALU'] = df['DS_SIT_ALU'].map(evasao)
 
 columns_mapping = ['DS_BAIRRO', 'DS_CIDADE', 'DS_ESTADO']
 for coluna in columns_mapping:
@@ -37,11 +37,11 @@ predictions = model.predict(X_test)
 accuracy = accuracy_score(y_test, predictions)
 
 print("Acurácia do Modelo:", accuracy)
-""""
+
 plt.figure(figsize=(15, 12))
 tree.plot_tree(model, filled=True, feature_names=X.columns, class_names=y['DS_SIT_ALU'].unique(), fontsize=8)
 plt.show()
-"""
+
 
 coluna1 = 'aluno'
 coluna2 = 'DS_BAIRRO'
@@ -54,13 +54,10 @@ y_df_test = y_test.values.reshape(-1, 1)
 
 print(X_df_test[y_df_test != y_pred])
 
-mapeamento = {'TRANSFERIDO', 'GRADUADO', 'CURSANDO', 'ABANDONO',  'DESISTENTE', 'CANCELADO'}
-colors = plt.cm.get_cmap('viridis', len(mapeamento))
-
 fig, ax = plt.subplots()
-#plt.scatter(x=X_train[coluna1], y=X_train[coluna2], c=y_df_train, alpha=0.9, cmap=colors)
-plt.scatter(x=X_test[coluna1], y=X_test[coluna2], c=y_pred, alpha=0.9, cmap=colors)
-plt.scatter(x=X_test[coluna1], y=X_test[coluna2], c=y_df_test, alpha=0.2, cmap=colors)
+#plt.scatter(x=X_train[coluna1], y=X_train[coluna2], c=y_df_train, alpha=0.9, cmap='viridis')
+plt.scatter(x=X_test[coluna1], y=X_test[coluna2], c=y_pred, alpha=0.9, cmap='viridis')
+plt.scatter(x=X_test[coluna1], y=X_test[coluna2], c=y_df_test, alpha=0.2, cmap='viridis')
 cbar = plt.colorbar()
 cbar.set_label('DS_SIT_ALU')
 
