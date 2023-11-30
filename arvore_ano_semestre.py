42fe13cc49ce0265b753c1b3dd4b7988788b3def
Natalia <naty-aragao@hotmail.com>
2023-11-30 00:32:44 -0300

    melhores regras de divisao da arvore

--- arvore_ano_semestre.py
+++ arvore_ano_semestre.py
@@ -10,25 +10,21 @@ from sklearn import tree
 import seaborn as sns
 
 df = pd.read_csv('situacao_aluno_2021-2023.csv')
-
-df = df.drop(['DT_SIT_ALU'], axis=1)
-
+df = df.drop(['aluno', 'DT_SIT_ALU', 'DS_BAIRRO'], axis=1)
 df = df.fillna(0)
 
-# Preenchendo dados faltosos com a mediana
+# Preenchendo dados faltosos com a media
 colunas_faltosas = ['N_COLOCA', 'N_TOTESC', 'N_NOTRED']
 for coluna in colunas_faltosas:
     media_sem_zeros = df[df[coluna] != 0][coluna].mean()
     df[coluna] = df[coluna].replace(0, round(media_sem_zeros))
 
-
 evasao = {'CURSANDO': 'NAO EVADIU', 'ABANDONO': 'EVADIU',  'DESISTENTE': 'EVADIU'}
 df['DS_SIT_ALU'] = df['DS_SIT_ALU'].map(evasao)
-
 mapeamento = {'NAO EVADIU': 1, 'EVADIU': 2}
-#df['DS_SIT_ALU'] = df['DS_SIT_ALU'].replace(mapeamento)
+df['DS_SIT_ALU'] = df['DS_SIT_ALU'].replace(mapeamento)
 
-columns_mapping = ['DS_BAIRRO', 'DS_CIDADE', 'DS_ESTADO']
+columns_mapping = ['DS_CIDADE', 'DS_ESTADO']
 for coluna in columns_mapping:
     label_encoder = preprocessing.LabelEncoder()
     df[coluna] = label_encoder.fit_transform(df[coluna])
@@ -39,6 +35,15 @@ df = df.loc[df['CD_SEM_INGRESSO'] == 1]
 
 X = df.iloc[:,1:]
 y = df.iloc[:,:1]
+
+def formatar_em_string(y):
+    y_string = []
+    for valor in y.values:
+        palavra = str(valor)
+        y_string.append(palavra)
+    y_string = np.array(y_string) 
+    return y_string
+y = formatar_em_string(y)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8, random_state=42)
 
@@ -53,15 +58,21 @@ print("Acurácia do Modelo:", accuracy)
 print("Resultados da Validacao Cruzada:", cross_val_results)
 print("Precisão Média: {:.2f}%".format(cross_val_results.mean() * 100))
 
+print("Melhores features da Árvore: ", model.feature_importances_)
+indices_ordenados = np.argsort(model.feature_importances_)[::-1]
+print("Características mais importantes:")
+for indice in indices_ordenados:
+    print(f"C {X_test.columns[indice]}: Importância = {model.feature_importances_[indice]}")
+
+
 def plot_arvore(arvore):
     plt.figure(figsize=(15, 12))
-    tree.plot_tree(arvore, filled=True, feature_names=X.columns, class_names=y['DS_SIT_ALU'].unique(), fontsize=8)
+    tree.plot_tree(arvore, filled=True, feature_names=X.columns, class_names=arvore.classes_, fontsize=8)
     plt.show()
 
-plot_arvore(model)
+#plot_arvore(model)
 
 def plot_confusao(matriz):
-    plt.figure(figsize=(10, 8))
     sns.heatmap(matriz, annot=True, fmt='d', cmap='Blues')
     plt.xlabel('Predições')
     plt.ylabel('Verdadeiro')
@@ -73,7 +84,7 @@ def plot_confusao(matriz):
     plt.show()
 
 matriz_confusao = confusion_matrix(y_test, predictions)
-#plot_confusao(matriz_confusao)
+plot_confusao(matriz_confusao)
 
 def scatter(conjunto, classe, col1, col2):
     fig, ax = plt.subplots()
@@ -85,7 +96,7 @@ def scatter(conjunto, classe, col1, col2):
     plt.title('Scatter Plot entre ' + col1 + ' e ' + col2)
     plt.show()
 
-coluna1 = 'DS_BAIRRO'
+coluna1 = 'num_idade'
 coluna2 = 'DS_CIDADE'
 y_pred = predictions.reshape(-1, 1)
 y_df_train = y_train.values.reshape(-1, 1)
@@ -94,7 +105,6 @@ X_df_train = X_train[[coluna1, coluna2]]
 X_df_test = X_test[[coluna1, coluna2]]
 
 #scatter(X_train, y_df_train, coluna1, coluna2)
-
 
 print(X_df_test[y_df_test != y_pred])
 print(X_df_test)
